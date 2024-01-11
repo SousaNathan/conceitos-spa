7de339852fb0f3e1814eacb9ce2376b615dcbc3e
Nathan Sousa <nathan.wks@outlook.com>
2024-01-10 23:37:18 -0300

    modulos e classe Route

--- src/index.js
+++ src/index.js
@@ -1,31 +1,12 @@
-const routes = {
-  "/": "pages/home.html",
-  "/about": "pages/about.html",
-  "/contact": "pages/contact.html",
-  404: "pages/404.html"
-}
+import { Router } from "./router.js"
 
-const route = (event) => {
-  event = event || window.event
-  event.preventDefault()
+const router = new Router()
+router.add("/", "/pages/home.html")
+router.add("/about", "/pages/about.html")
+router.add("/contact", "/pages/contact.html")
+router.add(404, "/pages/404.html")
 
-  window.history.pushState({}, '', event.target.href)
-  
-  handle()
-}
+router.handle()
 
-function handle() {
-  const { pathname } = window.location
-  const route = routes[pathname] || routes[404]
-
-  fetch(route)
-  .then(data => data.text())
-  .then(html => {
-    document.querySelector('#app').innerHTML = html
-  })
-}
-
-handle()
-
-window.onpopstate = () => handle()
-window.route = () => route()
+window.onpopstate = () => router.handle()
+window.route = () => router.route()
